53c657abf834f1be441b2271b8809af8b3a842be
unknown <peteyama.py@gmail.com>
2022-11-17 15:00:03 -0500

    Add images

--- src/component/card/nft.card.tsx
+++ src/component/card/nft.card.tsx
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from "react";
-import { Col } from "antd";
+import { Card, Col } from "antd";
 import { IListedNFT } from "../../type";
-
+import "./nft.card.css";
 type Props = {
   nft: IListedNFT;
 };
 
 const NFTCard: React.FC<Props> = ({ nft }) => {
   return (
-    <Col className="gutter-row" span={2}>
-      <h3>{nft.token_id}</h3>
-      <img src={nft.img} style={{ width: "100%" }}></img>
+    <Col className="gutter-row mb-10" span={2}>
+      <div className="nft-card">
+        <div className="nft-id">#{nft.token_id}</div>
+        <img
+          alt="example"
+          src={`${process.env.PUBLIC_URL}/image/nft/boredapeyc/${nft.token_id}.png`}
+          style={{ width: "100%" }}
+        />
+        <div className="nft-price">{nft.price.toFixed(3)}ETH</div>
+      </div>
     </Col>
   );
 };
--- src/page/nftlist.tsx
+++ src/page/nftlist.tsx
@@ -1,33 +1,56 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { Row } from "antd";
-import { commonState } from "../reducer/common.reducer";
+import { Row, Pagination } from "antd";
+import type { PaginationProps } from "antd";
+import { commonState, updateCommonState } from "../reducer/common.reducer";
 import { nftlistState, getAllListedNFTs } from "../reducer/nftlist.reducer";
 import { AppDispatch, AppSelector } from "../store";
-import { pageSize } from "../config/common.config";
+// import { pageSize } from "../config/common.config";
 import NFTCard from "../component/card/nft.card";
 
 const NFTList: React.FC = () => {
   const dispatch: AppDispatch = useDispatch();
 
-  const { nfttype, currentPage } = AppSelector(commonState);
+  const { nfttype } = AppSelector(commonState);
   const { allListedNFTs } = AppSelector(nftlistState);
 
+  const [pageSize, setPageSize] = useState(20);
+  const [pageNumber, setPageNumber] = useState(1);
   useEffect(() => {
     dispatch(getAllListedNFTs(String(nfttype)));
   }, []);
 
+  const handlePaginationChange: PaginationProps["onChange"] = (
+    pageNumber,
+    pageSize
+  ) => {
+    setPageNumber(pageNumber);
+    setPageSize(pageSize);
+  };
+
   return (
-    <Row>
-      {allListedNFTs
-        .slice(
-          pageSize * (currentPage.valueOf() - 1),
-          pageSize * currentPage.valueOf()
-        )
-        .map((nft) => (
-          <NFTCard nft={nft} />
-        ))}
-    </Row>
+    <>
+      <Row>
+        <Pagination
+          showQuickJumper
+          pageSize={pageSize}
+          defaultCurrent={1}
+          total={allListedNFTs.length}
+          onChange={handlePaginationChange}
+          style={{ float: "right" }}
+        />
+      </Row>
+      <Row gutter={16} style={{ padding: "10px" }}>
+        {allListedNFTs
+          .slice(
+            pageSize * (pageNumber.valueOf() - 1),
+            pageSize * pageNumber.valueOf()
+          )
+          .map((nft) => (
+            <NFTCard nft={nft} />
+          ))}
+      </Row>
+    </>
   );
 };
 
